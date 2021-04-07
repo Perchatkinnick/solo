3ec497491e25c3b4171b6f166bc9c0a87665a59a
Perchatkin Nick <koljaperchatkin@gmail.com>
2021-04-07 23:55:13 +1000

    Added some interractive in slider

--- scripts/index__script.js
+++ scripts/index__script.js
@@ -10,6 +10,20 @@ let data = [
 
 function onLoad() {
     galleryLoad();
+
+    let forward = document.querySelector('.gallery__forward');
+    forward.addEventListener('click', onBackClickHandler);
+}
+
+function onBackClickHandler() {
+    let activeSlide = document.querySelector('.gallery__slide__active');
+    activeSlide.classList.remove('gallery__slide__active');
+    let left = +activeSlide.style.left.split('px')[0];
+    let nextSlide = activeSlide.nextElementSibling;
+    nextSlide.classList.add('gallery__slide__active');
+    nextSlide.style.left = left + 220 + 'px';
+
+    loadMask();
 }
 
 function galleryLoad() {
@@ -21,17 +35,22 @@ function galleryLoad() {
         let slide = document.createElement('div');
 
         slide.classList.add('gallery__slide');
+        if (i == 0) {
+            slide.classList.add('gallery__slide__active');
+        }
+
         let number = i + 1;
 
         slide.style.background = ' url("media/images/' + number + '.jpg")';
 
-        if (i > 0) {
-            slide.style.background = 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("media/images/' + number + '.jpg")';
-        }
         slide.style.backgroundPosition = "-511px 0";
         if (i > 0) {
             slide.style.left = 631 + 260 * (i - 1) + 'px';
         }
+
+        let mask = document.createElement('div');
+        mask.classList.add('gallery__slide__mask');
+        slide.appendChild(mask);
 
         let title = document.createElement('div');
         title.classList.add('gallery__slide__title');
@@ -54,4 +73,21 @@ function galleryLoad() {
 
         conteiner.appendChild(slide);
     }
+
+    loadMask();
+}
+
+function loadMask() {
+    let conteiner = document.querySelector('.gallery');
+
+    let sliders = conteiner.getElementsByClassName('gallery__slide');
+    for (let slider of sliders) {
+        let mask = slider.querySelector('.gallery__slide__mask');
+        if (!slider.classList.contains('gallery__slide__active')) {
+            mask.style.opacity = '0.5';
+        } else {
+            mask.style.opacity = '0';
+        }
+    }
+
 }
